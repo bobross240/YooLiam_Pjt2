93644cf990f81d9876cd4be116b851d29dc2451f
Liam Yoo <lsyoo@wpi.edu>
2018-02-16 15:51:48 -0500

    i git u

--- CS4731_Proj2.js
+++ CS4731_Proj2.js
@@ -6,7 +6,6 @@ var program;
 
 var file;
 var fileString;
-var fudgeFactor = -0.1;
 var ply_vertices = 0;
 var ply_poly = 0;
 var isReading = false;
@@ -18,32 +17,92 @@ var fVertices = [];
 var triangles = [];
 var normals = [];
 
-var theta = 0;
+var thetaX = 0;
+var thetaY = 0;
+var thetaZ = 0;
+var thetaN = 0;
+var rotation = 0;
+var pulsing = false;
+var rotating = false;
+var direction = 0; //Not moving
+
+var near = -1.0;
+var far = 1;
+var radius = 4.0;
+var theta  = 90.0;
+var phi    =0.0;
+var dr = 5.0 * Math.PI/180.0;
+
+var  fovy = 60.0;  // Field-of-view in Y direction angle (in degrees)
+var  aspect;       // Viewport aspect ratio
+
+var mvMatrix, pMatrix, animationMatrix, rotationMatrix, transformMatrix, scaleMatrix;
+var modelView, projection;
+var rotationMatrixLocation, animationMatrixLocation, scaleMatrixLocation, translateMatrixLocation;
+var eye;
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
 
 // Renders one triangle
 // Input: 3 vertices
 var then = 0;
 
-function render_triangle(a, b, c)
+function render_triangles(vertices)
 {
-    points = [];
-    points.push(vec4(fVertices[a][0], fVertices[a][1], fVertices[a][2], 1));
-    points.push(vec4(fVertices[b][0], fVertices[b][1], fVertices[b][2], 1));
-    points.push(vec4(fVertices[c][0], fVertices[c][1], fVertices[c][2], 1));
+    for(var i = 0; i < vertices.length; i+=3) {
+        points = [];
+        points.push(vec4(fVertices[i][0], fVertices[i][1], fVertices[i][2], 1));
+        points.push(vec4(fVertices[i + 1][0], fVertices[i + 1][1], fVertices[i + 1][2], 1));
+        points.push(vec4(fVertices[i + 2][0], fVertices[i + 2][1], fVertices[i + 2][2], 1));
 
+        var pBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+        var vPosition = gl.getAttribLocation(program,  "vPosition");
+        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vPosition);
+
+        gl.drawArrays(gl.LINE_LOOP, 0, points.length); //Draw arrays into triangles
+    }
+}
+
+
+
+// Establishes the event listener for file loading
+function main(){
+
+    //Retrieve the <canvas> element
+    var canvas = document.getElementById('webgl');
+    document.getElementById('input').addEventListener('change', loadFile, false);
+    //Get the rendering context for WebGL
+    gl = WebGLUtils.setupWebGL(canvas, undefined);
+
+
+    //Check that the rendering context isn't null
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL.');
+        return;
+    }
+
+    //Initialize shaders
+    program = initShaders(gl, "vshader", "fshader");
+    gl.useProgram(program);
+
+    //Initialize viewport
+    gl.viewport(0, 0, 400, 400);
+    aspect = canvas.width/canvas.height;
+
+    gl.enable(gl.DEPTH_TEST);
+    //Set the clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     colors = [];
     colors.push(1.0, 1.0, 1.0, 1.0);
     colors.push(1.0, 1.0, 1.0, 1.0);
     colors.push(1.0, 1.0, 1.0, 1.0);
 
-    var pBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation(program,  "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
@@ -53,20 +112,10 @@ function render_triangle(a, b, c)
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
-
-    gl.drawArrays(gl.LINE_LOOP, 0, points.length); //Draw arrays into triangles
-}
-
-
-
-// Establishes the event listener for file loading
-function main(){
-
     var inputElement = document.getElementById('input'); //Get input element from the load file button
     inputElement.addEventListener('change', loadFile, false); //Add event listener that calls loadFile
 
-    var rotating = false;
-    var direction = 0; //Not moving
+
 
     document.addEventListener('keydown', function(event){
         if(event.keyCode == 88){
@@ -102,18 +151,18 @@ function main(){
         }
 
         if(event.keyCode == 90){
-            if(direction == 1){
-                direction = 0;
-            } else {
-                direction = 1;
-            }
-        }
-
-        if(event.keyCode == 65){
             if(direction == 3){
                 direction = 0;
             } else {
                 direction = 3;
+            }
+        }
+
+        if(event.keyCode == 65){
+            if(direction == -3){
+                direction = 0;
+            } else {
+                direction = -3;
             }
         }
 
@@ -132,40 +181,20 @@ function main(){
                 rotating = true;
             }
         }
+
+        if(event.keyCode == 66){
+            if(pulsing){
+                rotating = false;
+            } else {
+                rotating = true;
+            }
+        }
     });
 }
 
 // Iterates through aPoly (list of triangles) and renders them
 function render() {
-    // var ctMatrix = rotateX(theta);
-    // theta +=1;
-    //
-    // var modelMatrix = gl.getUniformLocation(program, 'modelMatrix');
-    // gl.uniformMatrix4fv(modelMatrix, false, flatten(ctMatrix));
-
-    //Retrieve the <canvas> element
-    var canvas = document.getElementById('webgl');
-    document.getElementById('input').addEventListener('change', loadFile, false);
-    //Get the rendering context for WebGL
-    gl = WebGLUtils.setupWebGL(canvas, undefined);
-
-    //Check that the rendering context isn't null
-    if (!gl) {
-        console.log('Failed to get the rendering context for WebGL.');
-        return;
-    }
-
-    //Initialize shaders
-    program = initShaders(gl, "vshader", "fshader");
-    gl.useProgram(program);
-
-    //Initialize viewport
-    gl.viewport(0, 0, 400, 400);
-
-    //Set the clear color
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.enable(gl.DEPTH_TEST);
+    eye = vec3(2, 2, 2);
 
     for (var j = 0; j < triangles.length; j++) {
         fVertices.push([triangles[j][0][0], triangles[j][0][1], triangles[j][0][2]]);
@@ -173,26 +202,60 @@ function render() {
         fVertices.push([triangles[j][2][0], triangles[j][2][1], triangles[j][2][2]]);
     }
 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
     var data = new plyData(fVertices);
 
-    var transformMatrix = translate(-data.midX, -data.midY, -data.midZ);
-    var scaleMatrix = scalem(1 / (data.width / 1.6), 1 / (data.height / 1.6), 1 / (data.depth / 1.6));
+    transformMatrix = translate(-data.midX, -data.midY, -data.midZ);
+    scaleMatrix = scalem(1 / (data.width / 1.1), 1 / (data.height / 1.1), 1 / (data.depth / 1.1));
 
-    var translateMatrixLocation = gl.getUniformLocation(program, 'translateMatrix');
-    var scaleMatrixLocation = gl.getUniformLocation(program, 'scaleMatrix');
+    if(direction == 1){
+        thetaX += 0.2;
+    }
+    if(direction == -1){
+        thetaX -= 0.2;
+    }
+    if(direction == 2){
+        thetaY += 0.2;
+    }
+    if(direction == -2){
+        thetaY -= 0.2;
+    }
+    if(direction == 3){
+        thetaZ += 0.2;
+    }
+    if(direction == -3){
+        thetaZ -= 0.2;
+    }
+    if(rotating){
+        rotation += 2;
+    }
 
+    mvMatrix = lookAt(eye, at, up);
+    pMatrix = perspective(fovy, aspect, -1, 1);
+    animationMatrix = translate(thetaX, thetaY, thetaZ);
+    rotationMatrix = rotate(rotation, [1, 0, 0]);
+
+    projection = gl.getUniformLocation(program, 'projectionMatrix');
+    modelView = gl.getUniformLocation(program, 'modelMatrix');
+
+    rotationMatrixLocation = gl.getUniformLocation(program, 'rotationMatrix');
+    animationMatrixLocation = gl.getUniformLocation(program, 'animationMatrix');
+    translateMatrixLocation = gl.getUniformLocation(program, 'translateMatrix');
+    scaleMatrixLocation = gl.getUniformLocation(program, 'scaleMatrix');
+
+    gl.uniformMatrix4fv(projection, false, flatten(pMatrix));
+    gl.uniformMatrix4fv(modelView, false, flatten(mvMatrix));
+    gl.uniformMatrix4fv(rotationMatrixLocation, false, flatten(rotationMatrix));
+    gl.uniformMatrix4fv(animationMatrixLocation, false, flatten(animationMatrix));
     gl.uniformMatrix4fv(scaleMatrixLocation, false, flatten(scaleMatrix));
     gl.uniformMatrix4fv(translateMatrixLocation, false, flatten(transformMatrix));
 
-    var fudgeLocation = gl.getUniformLocation(program, "fudgeFactor");
+    render_triangles(fVertices);
 
-    gl.uniform1f(fudgeLocation, fudgeFactor);
 
-    for (var k = 0; k < fVertices.length; k += 3) {
-        render_triangle(k, k + 1, k + 2);
-    }
-
-    //requestAnimationFrame(render);
+    requestAnimationFrame(render);
 }
 
 
@@ -254,11 +317,6 @@ function loadFile(event) {
                 isReading = true;
             }
         }
-
-        console.log("ply_vertices: " + ply_vertices);
-        console.log("ply_poly: " + ply_poly);
-        console.log("Vertices length: " + iVertices.length);
-        console.log("Triangles length: " + triangles.length);
 
         render();
     };
